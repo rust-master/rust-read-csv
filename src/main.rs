db6cf5a293cb4f27b244278f4b044e70035763a5
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-12-12 12:42:00 +0500

    read from csv file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use csv;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = read_from_file("./sample.csv"){
+        eprintln!("{}", e);
+    }
+}
+
+fn read_from_file(path: &str) -> Result<() , Box<dyn Error>>  {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record  = result?;
+
+        println!("{:?}", record);
+    }
+
+    Ok(())
 }
